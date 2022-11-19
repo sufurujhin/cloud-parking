f6679bdbc5632d0300fbb7082c9ff9b9bb065e5f
sufurujhin <daniel_pato@hotmail.com>
2022-11-19 20:22:38 -0300

    Estrutura inicial

---
+++ src/test/java/com/daniel/ferreira/CloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.ferreira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
